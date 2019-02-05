3da868c75d77a00a4353a85b38d5a347d644bdb9
hkKevin <pakkiuleung@gmail.com>
2019-02-05 21:58:23 +1100

    Applied Responsive layout in Movie info page.

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -6,7 +6,9 @@ import './Movie.css';
 import Header from '../Header/Header';
 
 class Movie extends Component {
+
   render() {
+
     const backdropSrc = 'https://image.tmdb.org/t/p/w780';
     const posterSrc = 'https://image.tmdb.org/t/p/w342';
     const profileSrc = 'https://image.tmdb.org/t/p/w185'
@@ -16,6 +18,17 @@ class Movie extends Component {
       if (this.props.info.id === this.props.movieId) {  // If new movie details loaded...
         const runtimeHour = Math.floor(this.props.info.runtime / 60);
         const runtimeMin = this.props.info.runtime % 60;
+
+        let director = null;
+        const directorArr = [];
+        if (this.props.credits) {
+          this.props.credits.crew.map(crew => {
+            if (crew.job === 'Director') {
+              directorArr.push(' ' + crew.name)
+            }
+          });
+          director = directorArr
+        }
 
         let cast = null;
         if (this.props.credits) {
@@ -78,48 +91,55 @@ class Movie extends Component {
 
         movieInfo = (
           <div className='grid'>
-            <div id='movieDetails'>
+            <div id='movieInfo'>
               <div id='movieIntro'>
-                <div id='left'>
-                  <div id='movieTitle'>
-                    {this.props.info.title}
-                  </div>
-                  <div id='yearRuntimeRating'>
-                    <span id='year'>{this.props.info.release_date.substring(0, 4)}</span>
-                    <div>
-                      {runtimeHour}h {runtimeMin}m
-                    </div>
-                    <div>
-                      <i className="far fa-star fa-xs"></i>
-                      {this.props.info.vote_average}
-                      <span id='denominator'> / 10</span>
-                      {/* <span id='voteCount'> ({this.props.info.vote_count})</span> */}
-                    </div>
-                  </div>
-
-                  {/* <p>Movie ID: {this.props.info.id}</p> */}
-                  <p>{this.props.info.overview}</p>
-                  <p>Budget: {this.props.info.budget}</p>
-                  <p>Revenue: {this.props.info.revenue}</p>
-
-                  <div id='castContainer'>
-                    {cast}
-                  </div>
-
+                <div id='movieTitle'>
+                  {this.props.info.title}
                 </div>
+                <div id='yearRuntimeRating'>
+                  <span id='year'>{this.props.info.release_date.substring(0, 4)}</span>
+                  <div>
+                    {runtimeHour}h {runtimeMin}m
+                    </div>
+                  <div>
+                    <i className="far fa-star fa-xs"></i>
+                    {this.props.info.vote_average}
+                    <span id='denominator'> / 10</span>
+                    {/* <span id='voteCount'> ({this.props.info.vote_count})</span> */}
+                  </div>
+                </div>
+              </div>
 
-                <div id='right'>
+              <div id='movieDetails'>
+                <div id='left'>
                   {this.props.info.poster_path
                     ? <img src={posterSrc + this.props.info.poster_path} title={this.props.info.title} alt={'Poster of "' + this.props.info.title + '"'} />
                     : <p>Poster of "{this.props.info.title}" not found</p>}
                 </div>
 
+                <div id='right'>
+                  {/* <p>Movie ID: {this.props.info.id}</p> */}
+                  <p>{this.props.info.overview}</p>
+
+                  <div id='budgetRevenueContainer'>
+                    <p>Budget: $ {this.props.info.budget.toLocaleString()}</p>
+                    <p>Revenue: $ {this.props.info.revenue.toLocaleString()}</p>
+                  </div>
+
+                  <div id='directorContainer'>
+                    {'Directed by ' + director}
+                  </div>
+
+                  <div id='castContainer'>
+                    {cast}
+                  </div>
+                  
+                </div>
               </div>
 
               <div id='videoContainer'>
                 {videos}
               </div>
-
 
               <Carousel
                 options={{
@@ -134,18 +154,18 @@ class Movie extends Component {
 
               <div>
                 {reviewText}
-                <Collapsible accordion defaultActiveKey={0}>
+                <Collapsible defaultActiveKey={0}>
                   {reviews}
                 </Collapsible>
               </div>
 
             </div>
           </div>
+
         );
       } else {
         movieInfo = 'LOADING...';
       }
-
 
     } else {
       movieInfo = 'Movie Details not found.';
