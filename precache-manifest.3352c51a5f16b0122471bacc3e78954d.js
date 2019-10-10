badfb9d984af300e6b560b28857d0682019b25cb
hkKevin <pakkiuleung@gmail.com>
2019-10-10 22:04:08 +1100

    Updates

--- precache-manifest.3352c51a5f16b0122471bacc3e78954d.js
+++ precache-manifest.3352c51a5f16b0122471bacc3e78954d.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/movie-world/static/js/runtime~main.83ea77d6.js"
   },
   {
-    "revision": "8de19781f01437ad07b2",
-    "url": "/movie-world/static/js/main.8de19781.chunk.js"
+    "revision": "e23936c1ce299da48c34",
+    "url": "/movie-world/static/js/main.e23936c1.chunk.js"
   },
   {
     "revision": "46ac59d1b70f21e60ec7",
     "url": "/movie-world/static/js/1.46ac59d1.chunk.js"
   },
   {
-    "revision": "8de19781f01437ad07b2",
-    "url": "/movie-world/static/css/main.78528533.chunk.css"
+    "revision": "e23936c1ce299da48c34",
+    "url": "/movie-world/static/css/main.dfe3a8ba.chunk.css"
   },
   {
     "revision": "46ac59d1b70f21e60ec7",
     "url": "/movie-world/static/css/1.a076fc2b.chunk.css"
   },
   {
-    "revision": "adf5539676b080be54a3416ac358aa00",
+    "revision": "184791862222499481147a2e941b11c9",
     "url": "/movie-world/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movie-world/precache-manifest.34db78b3f9228819bd33e1acb7f8b7de.js"
+  "/movie-world/precache-manifest.3352c51a5f16b0122471bacc3e78954d.js"
 );
 
 workbox.clientsClaim();
